68313427fb5d37c6e0f2216a262f967890c65588
Bart J <bart@delta10.nl>
2018-05-17 23:34:08 -0400

    Provide elementary mutations (#16)
    Provide elementary mutations

--- core/mutation.py
+++ core/mutation.py
@@ -1,43 +1,62 @@
 import graphene
+from core.lib import get_id
+from .models import Group
 from .types import GroupType
 
 class CreateGroup(graphene.Mutation):
     class Arguments:
-        name = graphene.String()
-        description = graphene.String()
-        is_open = graphene.Boolean()
+        name = graphene.String(required=True)
+        description = graphene.String(required=True)
 
     ok = graphene.Boolean()
     group = graphene.Field(lambda: GroupType)
 
-    def mutate(self, info, name, description, is_open):
-        group = GroupType(name=name, description=description, is_open=is_open)
-        ok = True
+    def mutate(self, info, name, description):
+        try:
+            group = Group.objects.create(name=name, description=description)
+            ok = True
+        except:
+            group = None
+            ok = False
+
         return CreateGroup(group=group, ok=ok)
 
 class UpdateGroup(graphene.Mutation):
     class Arguments:
-        guid = graphene.ID()
-        name = graphene.String()
-        description = graphene.String()
-        is_open = graphene.Boolean()
+        id = graphene.ID(required=True)
+        name = graphene.String(required=True)
+        description = graphene.String(required=True)
 
     ok = graphene.Boolean()
     group = graphene.Field(lambda: GroupType)
 
-    def mutate(self, info, name, description, is_open):
-        group = GroupType(name=name, description=description, is_open=is_open)
-        ok = True
+    def mutate(self, info, id, name, description):
+        try:
+            group = Group.objects.get(pk=get_id(id))
+            group.name = name
+            group.description = description
+            group.save()
+            ok = True
+        except:
+            group = None
+            ok = False
+
         return UpdateGroup(group=group, ok=ok)
 
 class DeleteGroup(graphene.Mutation):
     class Arguments:
-        guid = graphene.ID()
+        id = graphene.ID(required=True)
 
     ok = graphene.Boolean()
 
-    def mutate(self, info, guid):
-        ok = True
+    def mutate(self, info, id):
+        try:
+            group = Group.objects.get(pk=get_id(id))
+            group.delete()
+            ok = True
+        except:
+            ok = False
+
         return DeleteGroup(ok=ok)
 
 class Mutation(graphene.ObjectType):
