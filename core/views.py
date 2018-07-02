81a87846aeba08d33af1d51d83e2444ebff89990
Bart <bart@jeukendrup.nl>
2018-07-02 15:40:34 +0200

    Fixed typo

--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ def index(request):
     return JsonResponse({
         'app': 'backend2',
         'status': 200,
-        'description': 'Backend2 is working correctly. Visit /graphql/ for the GraphQL API, visit /oidc/authorize/ for login, visit /admin/ for the admin panel.'
+        'description': 'Backend2 is working correctly. Visit /graphql/ for the GraphQL API, visit /oidc/authenticate/ for login, visit /admin/ for the admin panel.'
     })
