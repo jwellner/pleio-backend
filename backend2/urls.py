f82805205c80e005bdd24630779f7ccfb5c6ebe3
Bart <bart@jeukendrup.nl>
2018-07-17 18:38:00 +0200

    Renamed files to file, moved BinaryFile handling to core

--- backend2/urls.py
+++ backend2/urls.py
@@ -3,12 +3,12 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 from core import admin
-from core.views import index
+from core.views import index, upload
 
 urlpatterns = [
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)), name='graphql'),
     path('oidc/', include('mozilla_django_oidc.urls')),
-    path('files/', include('files.urls')),
+    path('files/upload/', csrf_exempt(upload), name='upload'),
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name='index')
 ]
