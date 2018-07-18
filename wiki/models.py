0ecf649d8cda5e5de0d6555aaccaa0c906f9d648
JWEE <janwout@gmail.com>
2018-07-18 11:19:41 +0200

    removed duplicate created_at

--- wiki/models.py
+++ wiki/models.py
@@ -8,7 +8,5 @@ class Wiki(Object):
     title = models.CharField(max_length=256)
     description = models.TextField()
 
-    created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.title
