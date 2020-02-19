5872aea1dcfabc464ad1c318c837701abb8df765
Jeroen Wellner <jeroen@wllnr.nl>
2020-02-19 09:28:06 +0100

    Remove reversion startup commands

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -6,8 +6,5 @@ python /app/manage.py collectstatic --noinput
 # Compile messages
 python /app/manage.py compilemessages
 
-# Create initial revisions
-### python /app/manage.py createinitialrevisions
-
 # Start Gunicorn processes
 python manage.py runserver 0.0.0.0:8000
--- docker/start.sh
+++ docker/start.sh
@@ -9,9 +9,6 @@ python /app/manage.py compilemessages
 # Run migrations shared
 python /app/manage.py migrate_schemas
 
-# Create initial revisions
-python /app/manage.py createinitialrevisions
-
 # Start Gunicorn processes
 echo Starting uwsgi
 uwsgi --http :8000 --module backend2.wsgi --static-map /static=/app/static --static-map /media=/app/media
