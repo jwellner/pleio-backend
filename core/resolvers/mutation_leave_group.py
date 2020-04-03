405bcc09f8efd34866e72f0c22db889299ec5c5e
janwout <janwout@gmail.com>
2020-04-03 14:34:24 +0200

    check if leaving group is disabled and throw error on leave group mutation, so leaving a mandatory group is not possible

--- core/resolvers/mutation_leave_group.py
+++ core/resolvers/mutation_leave_group.py
@@ -1,7 +1,7 @@
 from graphql import GraphQLError
 from django.core.exceptions import ObjectDoesNotExist
 from core.models import Group
-from core.constances import NOT_LOGGED_IN, COULD_NOT_FIND, USER_NOT_MEMBER_OF_GROUP
+from core.constances import NOT_LOGGED_IN, COULD_NOT_FIND, USER_NOT_MEMBER_OF_GROUP, LEAVING_GROUP_IS_DISABLED
 from core.lib import remove_none_from_dict
 
 def resolve_leave_group(_, info, input):
@@ -17,6 +17,9 @@ def resolve_leave_group(_, info, input):
         group = Group.objects.get(id=clean_input.get("guid"))
     except ObjectDoesNotExist:
         raise GraphQLError(COULD_NOT_FIND)
+
+    if group.is_leaving_group_disabled:
+        raise GraphQLError(LEAVING_GROUP_IS_DISABLED)
 
     if not group.is_member(user):
         raise GraphQLError(USER_NOT_MEMBER_OF_GROUP)
