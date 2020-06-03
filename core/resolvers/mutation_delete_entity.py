8b4639a592f4fb4139ba4782899be134f41e6129
janwout <janwout@gmail.com>
2020-06-03 08:55:13 +0000

    beheer#3258 implement deleting group

--- core/resolvers/mutation_delete_entity.py
+++ core/resolvers/mutation_delete_entity.py
@@ -1,7 +1,7 @@
 from graphql import GraphQLError
 from django.core.exceptions import ObjectDoesNotExist
 from core.constances import NOT_LOGGED_IN, COULD_NOT_SAVE
-from core.models import Entity
+from core.models import Entity, Group
 from core.resolvers.mutation_delete_comment import resolve_delete_comment
 
 def resolve_delete_entity(_, info, input):
@@ -11,16 +11,26 @@ def resolve_delete_entity(_, info, input):
     if not user.is_authenticated:
         raise GraphQLError(NOT_LOGGED_IN)
 
+    entity = None
+
+    try:
+        entity = Group.objects.get(id=input.get("guid"))
+    except ObjectDoesNotExist:
+        pass
+
     try:
         entity = Entity.objects.get(id=input.get("guid"))
     except ObjectDoesNotExist:
+        pass
+
+    if not entity:
         # TODO: update frontend to use deleteComment
         # raise GraphQLError(COULD_NOT_FIND)
         return resolve_delete_comment(_, info, input)
 
     if not entity.can_write(user):
         raise GraphQLError(COULD_NOT_SAVE)
-    
+
     entity.delete()
 
     return {
