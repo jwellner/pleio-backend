4f6778a06e15457c09fcccb8f8688a9bbe77be5a
Jeroen Wellner <jeroen@wllnr.nl>
2020-06-09 13:16:05 +0200

    Remove hardcoded domain and make local links relative

--- elgg/management/commands/replace_links.py
+++ elgg/management/commands/replace_links.py
@@ -19,6 +19,8 @@ class Command(InteractiveTenantOption, BaseCommand):
     help = 'Replace links after import'
     import_id = None
     helpers = None
+    elgg_domain = None
+    tenant_domain = None
 
     def get_elgg_from_options_or_interactive(self, **options):
         all_elgg_sites = Instances.objects.using("elgg_control").all()
@@ -47,11 +49,14 @@ class Command(InteractiveTenantOption, BaseCommand):
     def add_arguments(self, parser):
         super().add_arguments(parser)
         parser.add_argument('--elgg', help='elgg database')
+        parser.add_argument('--elgg_domain', help='elgg domain')
 
     def handle(self, *args, **options):
         if not settings.RUN_AS_ADMIN_APP:
             self.stdout.write("Only run this command from admin instance.")
             return
+
+        self.elgg_domain = options.get("elgg_domain", None)
 
         elgg_instance = self.get_elgg_from_options_or_interactive(**options)
         tenant = self.get_tenant_from_options_or_interactive(**options)
@@ -75,6 +80,23 @@ class Command(InteractiveTenantOption, BaseCommand):
             self.stdout.write(f"Import not run for tenant {tenant.schema_name}. Exiting.")
             return False
 
+        elgg_site = ElggSitesEntity.objects.using(self.import_id).first()
+
+        if not self.elgg_domain:
+            matches = re.findall(r'http(?:s)?\:\/\/(.+)\/', elgg_site.url)
+            self.elgg_domain = matches[0]
+
+        self.tenant_domain = tenant.get_primary_domain().domain
+
+        if not self.elgg_domain:
+            self.stdout.write(f"Unable to get elgg domain. Exiting.")
+            return False
+
+        if not self.tenant_domain:
+            self.stdout.write(f"Unable to get primary tenant domain. Exiting.")
+            return False
+
+        self.stdout.write(f"Found elgg domain {self.elgg_domain} and tenant domain {self.tenant_domain}")
 
         self._replace_menu()
         self._replace_entity_description()
@@ -171,12 +193,13 @@ class Command(InteractiveTenantOption, BaseCommand):
 
     def _replace_links(self, text):
         # Testing: https://regex101.com/r/13zeJW/2
-        matches = re.findall(r'(((https:\/\/support\.pleio\.nl)|(^|(?<=[ \"\n]))\/)[\w\-\/]*\/(view|download)\/([0-9]+)\/[\w\-\.\/\?\%]*)', text)
+
+        matches = re.findall(rf'(((https:\/\/{re.escape(self.elgg_domain)})|(^|(?<=[ \"\n]))\/)[\w\-\/]*\/(view|download)\/([0-9]+)[\w\-\.\/\?\%]*)', text)
 
         for match in matches:
             link = match[0]
             new_link = link
-            ids = re.findall(r'/([0-9]+)/', link)
+            ids = re.findall(r'(?:view|download)\/([0-9]+)', link)
             for id in ids:
                 map_entity = GuidMap.objects.filter(id=id).first()
                 if map_entity:
@@ -184,5 +207,8 @@ class Command(InteractiveTenantOption, BaseCommand):
 
             if link != new_link:
                 text = text.replace(link, new_link)
+
+        # make all local links relative
+        text = text.replace(f"https://{self.elgg_domain}/", "/")
 
         return text
