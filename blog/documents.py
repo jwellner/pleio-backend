78b7943b041710c1fb3a01a37647b45bc144afc0
janwout <janwout@gmail.com>
2020-07-08 12:04:25 +0200

    implement custom tokenizer

--- blog/documents.py
+++ blog/documents.py
@@ -2,6 +2,28 @@ from django_elasticsearch_dsl import fields
 from django_elasticsearch_dsl.registries import registry
 from .models import Blog
 from core.documents import DefaultDocument
+from elasticsearch_dsl import analysis, analyzer, tokenizer
+
+
+custom_stop_filter = analysis.token_filter(
+    "custom_stop_filter",
+    type="stop",
+    stopwords=['_dutch_'],
+    ignore_case=True
+)
+
+
+ngram_analyzer = analyzer(
+    'ngram_analyzer',
+    tokenizer=tokenizer(
+        'description_ngram',
+        type='ngram',
+        min_gram=3,
+        max_gram=3,
+        token_chars=["letter", "digit", "punctuation", "symbol"]
+    ),
+    filter=['lowercase', custom_stop_filter]
+)
 
 
 @registry.register_document
@@ -10,6 +32,9 @@ class BlogDocument(DefaultDocument):
     tags = fields.ListField(fields.TextField())
     read_access = fields.ListField(fields.TextField())
     type = fields.KeywordField(attr="type_to_string")
+    description = fields.TextField(
+        analyzer=ngram_analyzer
+    )
 
     class Index:
         name = 'entities'
@@ -19,7 +44,6 @@ class BlogDocument(DefaultDocument):
 
         fields = [
             'title',
-            'description',
             'created_at',
             'updated_at'
         ]
