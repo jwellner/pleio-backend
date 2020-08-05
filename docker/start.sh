37151ea4fd316bc11cfe2ac44c9c32c108a20373
Jeroen Wellner <jeroen@wllnr.nl>
2020-08-05 12:02:10 +0200

    Remove static-map media from start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -8,4 +8,4 @@ python /app/manage.py migrate_schemas
 
 # Start Gunicorn processes
 echo Starting uwsgi
-uwsgi --http :8000 --wsgi-disable-file-wrapper --module backend2.wsgi --static-map /static=/app/static --static-map /media=/app/media
+uwsgi --http :8000 --wsgi-disable-file-wrapper --module backend2.wsgi --static-map /static=/app/static
