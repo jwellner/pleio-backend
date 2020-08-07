b57b2c780868bb8115dd027258325dd6f67c1c00
Jeroen Wellner <jeroen@wllnr.nl>
2020-08-07 11:31:33 +0200

    If you use Django with uwsgi, make sure to enable threads.

--- docker/start-admin.sh
+++ docker/start-admin.sh
@@ -8,4 +8,4 @@ python /app/manage.py migrate_schemas
 
 # Start Gunicorn processes
 echo Starting uwsgi
-uwsgi --http :8000 --wsgi-disable-file-wrapper --module backend2.wsgi --static-map /static=/app/static
+uwsgi --http :8000 --wsgi-disable-file-wrapper --module backend2.wsgi --static-map /static=/app/static --enable-threads
