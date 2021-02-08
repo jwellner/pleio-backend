eada814261c6fe8b2bdd8568afeaa33e1435e9bb
Jeroen Wellner <jeroen@wllnr.nl>
2021-02-08 16:35:31 +0100

    Make setting key unique and prevent race conditions in config

--- core/models/setting.py
+++ core/models/setting.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Setting(models.Model):
-    key = models.CharField(max_length=255)
+    key = models.CharField(max_length=255, unique=True)
     value = models.JSONField(null=True, blank=True, help_text="Please provide valid JSON data")
 
     def __str__(self):
