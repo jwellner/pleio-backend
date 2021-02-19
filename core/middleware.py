fdfc422bf46a593a08034e1edd5afc5719c92de5
janwout <janwout@gmail.com>
2021-02-19 12:13:25 +0100

    dont show walled garden when logged in

--- core/middleware.py
+++ core/middleware.py
@@ -124,9 +124,10 @@ class WalledGardenMiddleware:
         return any(public_url.match(url) for public_url in public_urls)
 
     def __call__(self, request):
-        if (
+        if request.user.is_authenticated:
+            pass
+        elif (
             config.IS_CLOSED
-            and not request.user.is_authenticated
             and not self.is_public_url(request.path_info)
         ) or (
             config.WALLED_GARDEN_BY_IP_ENABLED
