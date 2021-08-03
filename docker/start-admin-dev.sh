8e8ef7ed35098aa019cf34bd976b5d74b028dd20
Pim Polderman <info@pimpolderman.nl>
2021-08-03 07:47:39 +0000

    Add a Initcontainer that handles the db migrations

--- docker/start-admin-dev.sh
+++ docker/start-admin-dev.sh
@@ -4,8 +4,8 @@
 python /app/manage.py collectstatic --noinput
 
 # Run migrations shared on admin pod only
-# python /app/manage.py migrate_schemas
+python /app/manage.py migrate_schemas
 
-# Start Gunicorn processes 
+# Start Gunicorn processes
 echo Starting uwsgi
 uwsgi --ini /uwsgi-dev.ini
--- docker/start-admin.sh
+++ docker/start-admin.sh
@@ -3,9 +3,6 @@
 # Collect static
 python /app/manage.py collectstatic --noinput
 
-# Run migrations shared on admin pod only
-# python /app/manage.py migrate_schemas
-
 # Start Gunicorn processes
 echo Starting uwsgi
 uwsgi --ini /uwsgi-prod.ini
