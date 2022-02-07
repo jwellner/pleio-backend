76b80107ae741f1cd2727d9078a68768f6d3dea7
Esther <esther@pleio.nl>
2022-02-07 12:20:24 +0100

    Add tests

--- event/tests/test_resolve_add_entity.py
+++ event/tests/test_resolve_add_entity.py
@@ -12,7 +12,7 @@ from core.constances import ACCESS_TYPE
 from core.lib import datetime_isoformat
 from mixer.backend.django import mixer
 from graphql import GraphQLError
-from datetime import datetime
+from datetime import timezone
 
 class AddEventTestCase(FastTenantTestCase):
 
@@ -21,6 +21,25 @@ class AddEventTestCase(FastTenantTestCase):
         self.authenticatedUser = mixer.blend(User)
         self.group = mixer.blend(Group, owner=self.authenticatedUser, is_membership_on_request=False)
         self.group.join(self.authenticatedUser, 'owner')
+
+        self.eventPublic = Event.objects.create(
+            title="Test public event",
+            rich_description="JSON to string",
+            read_access=[ACCESS_TYPE.public],
+            write_access=[ACCESS_TYPE.user.format(self.authenticatedUser.id)],
+            owner=self.authenticatedUser,
+            start_date=timezone.now()
+        )
+
+        self.eventGroupPublic = Event.objects.create(
+            title="Test public event",
+            rich_description="JSON to string",
+            read_access=[ACCESS_TYPE.public],
+            write_access=[ACCESS_TYPE.user.format(self.authenticatedUser.id)],
+            owner=self.authenticatedUser,
+            group=self.group,
+            start_date=timezone.now()
+        )
 
         self.data = {
             "input": {
@@ -111,3 +130,50 @@ class AddEventTestCase(FastTenantTestCase):
         self.assertEqual(data["addEntity"]["entity"]["richDescription"], variables["input"]["richDescription"])
         self.assertEqual(data["addEntity"]["entity"]["inGroup"], True)
         self.assertEqual(data["addEntity"]["entity"]["group"]["guid"], self.group.guid)
+
+    def test_add_event_to_parent(self):
+
+        variables = self.data
+        variables["input"]["containerGuid"] = self.eventPublic.guid
+
+        request = HttpRequest()
+        request.user = self.authenticatedUser
+
+        result = graphql_sync(schema, { "query": self.mutation, "variables": variables }, context_value={ "request": request })
+
+        data = result[1]["data"]
+
+        self.assertEqual(data["addEntity"]["entity"]["title"], variables["input"]["title"])
+        self.assertEqual(data["addEntity"]["entity"]["richDescription"], variables["input"]["richDescription"])
+        self.assertEqual(data["addEntity"]["entity"]["hasChildren"], False)
+        self.assertEqual(data["addEntity"]["entity"]["parent"]["guid"], self.eventPublic.guid)
+
+        self.eventPublic.refresh_from_db()
+
+        self.assertTrue(self.eventPublic.has_children())
+        self.assertEqual(self.eventPublic.children.first().guid, data["addEntity"]["entity"]["guid"])
+
+
+    def test_add_event_to_parent_with_group(self):
+
+        variables = self.data
+        variables["input"]["containerGuid"] = self.eventGroupPublic.guid
+
+        request = HttpRequest()
+        request.user = self.authenticatedUser
+
+        result = graphql_sync(schema, { "query": self.mutation, "variables": variables }, context_value={ "request": request })
+
+        data = result[1]["data"]
+
+        self.assertEqual(data["addEntity"]["entity"]["title"], variables["input"]["title"])
+        self.assertEqual(data["addEntity"]["entity"]["richDescription"], variables["input"]["richDescription"])
+        self.assertEqual(data["addEntity"]["entity"]["hasChildren"], False)
+        self.assertEqual(data["addEntity"]["entity"]["inGroup"], True)
+        self.assertEqual(data["addEntity"]["entity"]["group"]["guid"], self.group.guid)
+        self.assertEqual(data["addEntity"]["entity"]["parent"]["guid"], self.eventGroupPublic.guid)
+
+        self.eventGroupPublic.refresh_from_db()
+
+        self.assertTrue(self.eventGroupPublic.has_children())
+        self.assertEqual(self.eventGroupPublic.children.first().guid, data["addEntity"]["entity"]["guid"])
