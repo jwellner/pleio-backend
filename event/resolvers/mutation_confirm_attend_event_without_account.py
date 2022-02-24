68ea1c45b1d5a4deefe4fed7da2934ca04f9d56c
janwout <janwout@gmail.com>
2022-02-24 11:05:00 +0000

    beheer#9328 add state options to event confirm without account

--- event/resolvers/mutation_confirm_attend_event_without_account.py
+++ event/resolvers/mutation_confirm_attend_event_without_account.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy
-from core.constances import ALREADY_REGISTERED, COULD_NOT_FIND, INVALID_EMAIL, EVENT_IS_FULL, COULD_NOT_SAVE
+from core.constances import COULD_NOT_FIND, INVALID_EMAIL, EVENT_INVALID_STATE, EVENT_IS_FULL, COULD_NOT_SAVE
 from core.lib import remove_none_from_dict, get_base_url, get_default_email_context
 from event.models import Event, EventAttendeeRequest, EventAttendee
 from event.lib import get_url
@@ -13,10 +13,15 @@ from django_tenants.utils import parse_tenant_config_path
 def resolve_confirm_attend_event_without_account(_, info, input):
     # pylint: disable=redefined-builtin
     # pylint: disable=unused-argument
-
     clean_input = remove_none_from_dict(input)
+
     email = clean_input.get("email")
     delete = clean_input.get("delete")
+    #TODO: set default value for backwards compatibility, remove if frontend is altered
+    state = clean_input.get("state", "accept")
+
+    if state not in ["accept", "reject", "maybe", "waitinglist"]:
+        raise GraphQLError(EVENT_INVALID_STATE)
 
     try:
         event = Event.objects.get(id=clean_input.get("guid"))
@@ -47,20 +52,22 @@ def resolve_confirm_attend_event_without_account(_, info, input):
     # create attendee
     else:   
         try:
-            EventAttendee.objects.get(event=event, email=email)
-            raise GraphQLError(ALREADY_REGISTERED)
+            attendee = event.attendees.get(email=email)
         except ObjectDoesNotExist:
-            pass
+            attendee = None
 
-        if event.is_full():
-            raise GraphQLError(EVENT_IS_FULL)
+        if not attendee:
+            attendee = EventAttendee()
+            attendee.name = attendee_request.name
+            attendee.event = event
+            attendee.email = email
 
-        EventAttendee.objects.create(
-            name=attendee_request.name,
-            email=email,
-            event=event,
-            state='accept'
-        )
+        if state == "accept":
+            if event.is_full():
+                raise GraphQLError(EVENT_IS_FULL)
+
+        attendee.state = state
+        attendee.save()
 
         leave_url = get_base_url() + '/events/confirm/' + event.guid + '?email=' + email + '&code='
         leave_link = leave_url + attendee_request.code + '&delete=true'
@@ -75,8 +82,12 @@ def resolve_confirm_attend_event_without_account(_, info, input):
         context['title'] = event.title
         context['location'] = event.location
         context['start_date'] = event.start_date
+        context['state'] = state
 
         send_mail_multi.delay(schema_name, subject, 'email/confirm_attend_event_without_account.html', context, email)
+
+        if state != "accept":
+            event.process_waitinglist()
 
     return {
         "entity": event
