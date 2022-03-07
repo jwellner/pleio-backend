9c841b5c3bde3701797f49f57c6517084f70904b
Erlend ter Maat <erlend@pleio.nl>
2022-03-07 12:29:19 +0100

    beheer#3314: Verstuur mails over een event volgens de specificatie

--- event/resolvers/mutation_messages.py
+++ event/resolvers/mutation_messages.py
@@ -1,7 +1,13 @@
+from django.utils import translation
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy
+from django_tenants.utils import parse_tenant_config_path
 from graphql import GraphQLError
 
 from core.constances import COULD_NOT_FIND, NOT_AUTHORIZED
-from event.models import Event
+from core.lib import remove_none_from_dict, get_default_email_context
+from core.tasks import send_mail_multi
+from event.models import Event, EventAttendee
 
 
 def resolve_send_message_to_event(_, info, input):
@@ -12,11 +18,64 @@ def resolve_send_message_to_event(_, info, input):
         if not event.can_write(user):
             raise GraphQLError(NOT_AUTHORIZED)
 
-        # TODO: send mail.
+        clean_input = remove_none_from_dict(input)
 
-        return {'success': True}
+        receiving_users = []
+        if clean_input.get('isTest'):
+            receiving_users.append(user)
+        else:
+            if clean_input.get('sendToAttendees'):
+                for attendee in EventAttendee.objects.filter(event=event, state='accept'):
+                    receiving_users.append(attendee.user)
+
+        messenger = SendEventMessage()
+        messenger.populate(event=event,
+                           sender=user,
+                           message=clean_input.get('message'),
+                           subject=clean_input.get('subject'))
+
+        for receiving_user in receiving_users:
+            messenger.send(receiving_user=receiving_user,
+                           copy=False)
+
+        if clean_input.get('sendCopyToSender', False) and user not in receiving_users:
+            messenger.send(receiving_user=user,
+                           copy=True)
+
+        return {'success': True,
+                'messageCount': messenger.messageCount}
 
     except Event.DoesNotExist:
         raise GraphQLError(COULD_NOT_FIND)
 
-    return {'success': False}
+
+class SendEventMessage(object):
+
+    def __init__(self):
+        self.messageCount = 0
+
+    def populate(self, event, sender, message, subject):
+        self.event = event
+        self.sender = sender
+        self.context = get_default_email_context(sender)
+        self.schema_name = parse_tenant_config_path("")
+        self.context['message'] = format_html(message)
+        self.subject = subject
+
+    def send(self, receiving_user, copy: bool):
+        translation.activate(receiving_user.get_language())
+        if copy:
+            subject = ugettext_lazy("Message from event {0}: {1}").format(self.event.title, self.subject)
+        else:
+            subject = ugettext_lazy("Copy: Message from event {0}: {1}").format(self.event.title, self.subject)
+
+        self.messageCount = self.messageCount + 1
+
+        send_mail_multi.delay(
+            self.schema_name,
+            subject,
+            'email/send_message_to_event.html',
+            self.context,
+            receiving_user.email,
+            language=receiving_user.get_language()
+        )
--- event/tests/test_event_mailer.py
+++ event/tests/test_event_mailer.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from unittest import mock
 
 from ariadne import graphql_sync
 from django.http import HttpRequest
@@ -18,24 +19,40 @@ class EventsTestCase(FastTenantTestCase):
         self.mutation = """
             mutation ($input: sendMessageToEventInput!) {
                 sendMessageToEvent(input: $input) {
-                    success
+                    success,
+                    messageCount
                 }
             }
             """
 
         self.group = mixer.blend(Group)
-        self.event = mixer.blend(Event, group=self.group)
-
-        self.attendee = mixer.blend(User)
-        self.group.join(self.attendee)
-
         self.owner = mixer.blend(User)
-        self.group.join(self.owner)
-
+        self.event = mixer.blend(Event, group=self.group, owner=self.owner)
         self.event.write_access = [ACCESS_TYPE.user.format(self.owner.id)]
         self.event.save()
 
-        self.event_attendee = mixer.blend(EventAttendee, event=self.event, user=self.attendee)
+        self.attending_user = mixer.blend(User)
+        self.event_attendee = mixer.blend(EventAttendee,
+                                          event=self.event, user=self.attending_user, state='accept')
+
+        self.waiting_user = mixer.blend(User)
+        self.event_waiting_attendee = mixer.blend(EventAttendee,
+                                                  event=self.event, user=self.waiting_user, state='waitinglist')
+
+        self.maybe_attending_user = mixer.blend(User)
+        self.event_maybe_attendee = mixer.blend(EventAttendee,
+                                                event=self.event, user=self.maybe_attending_user, state='maybe')
+
+        self.rejected_user = mixer.blend(User)
+        self.event_rejected_attendee = mixer.blend(EventAttendee,
+                                                   event=self.event, user=self.rejected_user, state='reject')
+
+        self.second_attendee = mixer.blend(EventAttendee,
+                                           event=self.event, user=mixer.blend(User), state='accept')
+        self.third_attendee = mixer.blend(EventAttendee,
+                                          event=self.event, user=mixer.blend(User), state='accept')
+
+        self.outsider = mixer.blend(User)
 
     def test_event_mail_attendees_access(self):
         variables = {
@@ -43,9 +60,8 @@ class EventsTestCase(FastTenantTestCase):
                 'guid': self.event.guid,
                 'subject': "expected subject",
                 'message': "expected message",
-                'sendTest': True,
+                'isTest': True,
                 'sendToAttendees': True,
-                'sendToWaitinglist': True,
                 'sendCopyToSender': True,
             }
         }
@@ -62,13 +78,183 @@ class EventsTestCase(FastTenantTestCase):
         # Test attendee.
         with suppress_stdout():
             request = HttpRequest()
-            request.user = self.attendee
+            request.user = self.attending_user
             success, result = graphql_sync(schema, {"query": self.mutation,
                                                     "variables": variables},
                                            context_value={"request": request})
 
         self.assertIn('errors', result,
                       msg="graphql geeft aan dat er geen fouten zijn, maar een attendee mag helemaal geen mail sturen.")
+
+    @mock.patch('event.resolvers.mutation_messages.SendEventMessage.populate')
+    @mock.patch('event.resolvers.mutation_messages.SendEventMessage.send')
+    def test_event_mailer_should_send_test_mail_to_sender(self, mocked_sender_send, mocked_sender_prepare):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+        self.assertTrue('errors' not in result, msg=result)
+        mocked_sender_prepare.assert_called_with(event=self.event,
+                                                 sender=self.owner,
+                                                 message=variables['input']['message'],
+                                                 subject=variables['input']['subject'])
+        mocked_sender_send.assert_called_with(receiving_user=self.owner,
+                                              copy=False)
+
+    @mock.patch('event.resolvers.mutation_messages.SendEventMessage.send')
+    def test_event_mailer_should_send_test_mail_to_sender_only(self, mocked_sender_send):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': True,
+                'sendToAttendees': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+        self.assertTrue('errors' not in result, msg=result)
+
+        mocked_sender_send.assert_called_once_with(receiving_user=self.owner,
+                                                   copy=False)
+
+    @mock.patch('event.resolvers.mutation_messages.SendEventMessage.send')
+    def test_event_mailer_should_send_mail_to_attendees(self, mocked_sender_send):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': False,
+                'sendToAttendees': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+        self.assertTrue('errors' not in result, msg=result)
+
+        mocked_sender_send.assert_any_call(receiving_user=self.attending_user,
+                                           copy=False)
+        mocked_sender_send.assert_any_call(receiving_user=self.second_attendee.user,
+                                           copy=False)
+        mocked_sender_send.assert_any_call(receiving_user=self.third_attendee.user,
+                                           copy=False)
+
+    @mock.patch('event.resolvers.mutation_messages.SendEventMessage.send')
+    def test_event_mailer_should_send_copy_to_sender(self, mocked_sender_send):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': False,
+                'sendCopyToSender': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+        self.assertTrue('errors' not in result, msg=result)
+
+        mocked_sender_send.assert_called_once_with(receiving_user=self.owner,
+                                                   copy=True)
+
+    def test_event_mailer_should_send_one_test_message(self):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': True,
+                'sendToAttendees': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+
+        self.assertTrue('errors' not in result, msg=result)
+        self.assertEqual(result['data']['sendMessageToEvent']['messageCount'], 1)
+
+    def test_event_mailer_should_give_feedback_about_multiple_attendees(self):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': False,
+                'sendToAttendees': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+
+        self.assertTrue('errors' not in result, msg=result)
+        self.assertEqual(result['data']['sendMessageToEvent']['messageCount'], 3)
+
+    def test_event_mailer_should_give_feedback_about_multiple_attendees_and_a_copy(self):
+        request = HttpRequest()
+        request.user = self.owner
+
+        variables = {
+            'input': {
+                'guid': self.event.guid,
+                'subject': "expected subject",
+                'message': "expected message",
+                'isTest': False,
+                'sendToAttendees': True,
+                'sendCopyToSender': True,
+            }
+        }
+
+        success, result = graphql_sync(schema, {"query": self.mutation,
+                                                "variables": variables},
+                                       debug=False,
+                                       context_value={"request": request})
+
+        self.assertTrue('errors' not in result, msg=result)
+        self.assertEqual(result['data']['sendMessageToEvent']['messageCount'], 4)
 
 
 @contextmanager
