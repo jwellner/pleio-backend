e017c7aed7937d64af7f6b8b6b287087dfa74b51
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-05-10 10:37:37 +0000

    beheer#9219 Query toevoegen voor groepsleden (PENDING FE)

--- core/resolvers/query.py
+++ core/resolvers/query.py
@@ -3,40 +3,42 @@ from django.core.exceptions import ObjectDoesNotExist
 from graphql import GraphQLError
 from core.models import Entity, Group, EntityView, EntityViewCount
 from user.models import User
-from .query_viewer import resolve_viewer
-from .query_site import resolve_site, resolve_site_settings, resolve_site_stats
-from .query_site_users import resolve_site_users
-from .query_entities import resolve_entities
-from .query_groups import resolve_groups
 from .query_bookmarks import resolve_bookmarks
-from .query_search import resolve_search
-from .query_users import resolve_users
-from .query_users_by_birth_date import resolve_users_by_birth_date
+from .query_entities import resolve_entities
 from .query_filters import resolve_filters
-from .query_trending import resolve_trending
+from .query_groups import resolve_groups
+from .query_members import resolve_members
 from .query_notifications import resolve_notifications
 from .query_recommended import resolve_recommended
+from .query_search import resolve_search
+from .query_site import resolve_site, resolve_site_settings, resolve_site_stats
+from .query_site_users import resolve_site_users
 from .query_top import resolve_top
+from .query_trending import resolve_trending
+from .query_users import resolve_users
+from .query_users_by_birth_date import resolve_users_by_birth_date
+from .query_viewer import resolve_viewer
 from core.constances import USER_NOT_MEMBER_OF_GROUP, USER_ROLES
 
 query = ObjectType("Query")
 
-query.set_field("viewer", resolve_viewer)
+query.set_field("bookmarks", resolve_bookmarks)
+query.set_field("entities", resolve_entities)
+query.set_field("filters", resolve_filters)
+query.set_field("groups", resolve_groups)
+query.set_field("members", resolve_members)
+query.set_field("notifications", resolve_notifications)
+query.set_field("recommended", resolve_recommended)
+query.set_field("search", resolve_search)
 query.set_field("site", resolve_site)
 query.set_field("siteSettings", resolve_site_settings)
 query.set_field("siteStats", resolve_site_stats)
 query.set_field("siteUsers", resolve_site_users)
-query.set_field("entities", resolve_entities)
-query.set_field("groups", resolve_groups)
-query.set_field("bookmarks", resolve_bookmarks)
-query.set_field("search", resolve_search)
+query.set_field("top", resolve_top)
+query.set_field("trending", resolve_trending)
 query.set_field("users", resolve_users)
 query.set_field("usersByBirthDate", resolve_users_by_birth_date)
-query.set_field("notifications", resolve_notifications)
-query.set_field("trending", resolve_trending)
-query.set_field("recommended", resolve_recommended)
-query.set_field("top", resolve_top)
-query.set_field("filters", resolve_filters)
+query.set_field("viewer", resolve_viewer)
 
 
 @query.field("entity")
