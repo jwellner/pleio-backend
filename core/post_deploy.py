06f03a0a14596e9ea687844f68c7b708b6278ac2
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-06-23 13:20:32 +0000

    beheer#838 Migrationscript to copy site configuration setting into group setting

--- core/post_deploy.py
+++ core/post_deploy.py
@@ -1,7 +1,9 @@
 from django_tenants.utils import parse_tenant_config_path
 from post_deploy import post_deploy_action
 
+from core import config
 from core.elasticsearch import schedule_index_document
+from core.models import Group
 from user.models import User
 
 
@@ -12,3 +14,17 @@ def schedule_index_users():
 
     for user in User.objects.filter(is_active=True):
         schedule_index_document(user)
+
+
+@post_deploy_action
+def sync_is_submit_updates_enabled_group_setting():
+    """
+    Copy the site setting for submit-updates in groups into the group setting.
+    """
+    if parse_tenant_config_path("") == 'public':
+        return
+
+    site_setting = config.STATUS_UPDATE_GROUPS
+    for group in Group.objects.all():
+        group.is_submit_updates_enabled = site_setting
+        group.save()
