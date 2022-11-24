02204e270807d4f2f5bdd16a9792f46bad33a499
Jeroen Wellner <jeroen@wllnr.nl>
2022-11-24 20:21:23 +0000

    beheer#11256 serve gzipped static css and js files

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,9 @@
 # Collect static
 python /app/manage.py collectstatic --noinput
 
+# Gzip js and css
+yes n | gzip -k static/**/*.{js,css}
+
 # Start Gunicorn processes
 echo Starting uwsgi
 uwsgi --ini /uwsgi-prod.ini
