0a5074ba93520399f1cccba503ab1030062abef9
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-12-07 14:49:25 +0000

    beheer#1861 Add Member.memberSince to graphql schema.

--- core/tests/test_query_memberships.py
+++ core/tests/test_query_memberships.py
@@ -11,11 +11,11 @@ class FilterUsersTestCase(ElasticsearchTestCase):
     def setUp(self):
         super().setUp()
 
-        self.owner = mixer.blend(User, name="Owner")
+        self.owner = UserFactory(name="Owner", email='owner@example.com')
         self.group = mixer.blend(Group, owner=self.owner, introduction='introductionMessage')
         self.group.join(self.owner, 'owner')
 
-        self.admin = mixer.blend(User, name="Admin")
+        self.admin = UserFactory(name="Admin", email='admin@example.com')
         self.group.join(self.admin, 'admin')
 
         self.profile_field, created = ProfileField.objects.get_or_create(key='multi_key', is_filter=True,
@@ -30,7 +30,7 @@ class FilterUsersTestCase(ElasticsearchTestCase):
             show_field=True
         )
 
-        self.member1 = mixer.blend(User, name="Member 1")
+        self.member1 = mixer.blend(User, name="Member 1", email='member1@example.com')
         self.group.join(self.member1, 'member')
         self.profile1, created = UserProfile.objects.get_or_create(user=self.member1)
         self.user_profile_field1, created = UserProfileField.objects.get_or_create(
@@ -38,7 +38,7 @@ class FilterUsersTestCase(ElasticsearchTestCase):
             profile_field=self.profile_field,
             value='select_value_1')
 
-        self.member2 = mixer.blend(User, name="Member 2")
+        self.member2 = mixer.blend(User, name="Member 2", email='member2@example.com')
         self.group.join(self.member2, 'member')
         self.profile2, created = UserProfile.objects.get_or_create(user=self.member2)
         self.user_profile_field2, created = UserProfileField.objects.get_or_create(
@@ -46,13 +46,37 @@ class FilterUsersTestCase(ElasticsearchTestCase):
             profile_field=self.profile_field,
             value='select_value_2')
 
-        self.member3 = mixer.blend(User, name="Member 3")
+        self.member3 = mixer.blend(User, name="Member 3", email='member3@example.com')
         self.group.join(self.member3, 'member')
 
-        self.pending_member = mixer.blend(User, name="Pending Member")
+        self.pending_member = mixer.blend(User, name="Pending Member", email='pending@example.com')
         self.group.join(self.pending_member, 'pending')
 
-        self.not_a_member = mixer.blend(User, name="Outsider")
+        self.not_a_member = mixer.blend(User, name="Outsider", email='outsider@example.com')
+
+        self.query = """
+            query MembersQuery ($groupGuid: String!
+                                $query: String
+                                $filters: [FilterInput]
+                                $offset: Int
+                                $limit: Int) {
+                members(groupGuid: $groupGuid
+                        q: $query
+                        filters: $filters
+                        offset: $offset
+                        limit: $limit) {
+                    total
+                    edges {
+                        role
+                        email
+                        memberSince
+                        user {
+                            guid
+                        }
+                    }
+                }
+            }
+        """
 
     def teardown(self):
         self.owner.delete()
@@ -77,158 +101,81 @@ class FilterUsersTestCase(ElasticsearchTestCase):
         return result.get('data')
 
     def test_query_should_fail_for_anonymous_visitors(self):
-        query = """
-            query MembersQuery ($groupGuid: String!) {
-                members(groupGuid: $groupGuid) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
         variables = {
             "groupGuid": str(self.group.guid)
         }
 
         with self.assertGraphQlError("not_logged_in"):
-            self.graphql_client.post(query, variables)
+            self.graphql_client.post(self.query, variables)
 
     def test_query_should_fail_for_non_members(self):
-        query = """
-            query MembersQuery ($groupGuid: String!) {
-                members(groupGuid: $groupGuid) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
         variables = {
             "groupGuid": str(self.group.guid)
         }
 
         with self.assertGraphQlError("user_not_member_of_group"):
             self.graphql_client.force_login(self.not_a_member)
-            self.graphql_client.post(query, variables)
+            self.graphql_client.post(self.query, variables)
 
     def test_query_should_give_enhanced_response(self):
-        query = """
-            query MembersQuery ($groupGuid: String!) {
-                members(groupGuid: $groupGuid) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
         variables = {
             "groupGuid": str(self.group.guid)
         }
 
-        self.initialize_index()
-
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
+        members = data['members']['edges']
 
         self.assertEqual(data['members']['total'], 5)
-        self.assertEqual(len(data['members']['edges']), 5)
-        self.assertEqual(data['members']['edges'][0]['role'], 'owner',
-                         msg="Onverwachte volgorde van het resultaat: begint niet met de eigenaar.")
-        self.assertEqual(data['members']['edges'][1]['role'], 'admin',
-                         msg="Onverwachte volgorde van het resultaat: 2e is niet de admin.")
-        self.assertEqual(data['members']['edges'][2]['role'], 'member',
-                         msg="Onverwachte volgorde van het resultaat: 3e is niet member.")
+        self.assertEqual(len(members), 5)
+        owner = self.owner.memberships.get(group=self.group)
+        self.assertEqual(members[0]['role'], "owner")
+        self.assertEqual(members[0]['email'], owner.user.email)
+        self.assertEqual(members[0]['memberSince'], owner.created_at.isoformat())
+        admin = self.admin.memberships.get(group=self.group)
+        self.assertEqual(members[1]['role'], "admin")
+        self.assertEqual(members[1]['email'], admin.user.email)
+        self.assertEqual(members[1]['memberSince'], admin.created_at.isoformat())
+        member1 = self.member1.memberships.get(group=self.group)
+        self.assertEqual(members[2]['role'], "member")
+        self.assertEqual(members[2]['email'], member1.user.email)
+        self.assertEqual(members[2]['memberSince'], member1.created_at.isoformat())
+        member2 = self.member2.memberships.get(group=self.group)
+        self.assertEqual(members[3]['role'], "member")
+        self.assertEqual(members[3]['email'], member2.user.email)
+        self.assertEqual(members[3]['memberSince'], member2.created_at.isoformat())
+        member3 = self.member3.memberships.get(group=self.group)
+        self.assertEqual(members[4]['role'], "member")
+        self.assertEqual(members[4]['email'], member3.user.email)
+        self.assertEqual(members[4]['memberSince'], member3.created_at.isoformat())
 
     def test_full_text_query_should_ignore_non_members(self):
-        query = """
-            query MembersQuery ($groupGuid: String!, $query: String) {
-                members(groupGuid: $groupGuid, q: $query) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
         self.not_a_member.name = self.not_a_member.guid
         self.not_a_member.save()
-
-        self.initialize_index()
 
         variables = {
             "groupGuid": str(self.group.guid),
             "query": self.not_a_member.name
         }
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
 
         self.assertEqual(data['members']['total'], 0,
                          msg="Resultaat gevonden, not-a-member lijkt tussen de members te staan.")
 
     def test_full_text_query_should_include_members(self):
-        query = """
-            query MembersQuery ($groupGuid: String!, $query: String) {
-                members(groupGuid: $groupGuid, q: $query) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
         self.member1.name = self.member1.guid
         self.member1.save()
-
-        self.initialize_index()
 
         variables = {
             "groupGuid": str(self.group.guid),
             "query": self.member1.name
         }
 
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
 
         self.assertEqual(data['members']['total'], 1, msg="We verwachten 1 resultaat bij zoeken naar leden met een specifieke naam.")
         self.assertEqual(data['members']['edges'][0]['user']['guid'], str(self.member1.id))
 
     def test_query_should_find_profile_field_match(self):
-        query = """
-            query MembersQuery ($groupGuid: String!, $filters: [FilterInput]) {
-                members(groupGuid: $groupGuid, filters: $filters) {
-                    total
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-
-        self.initialize_index()
-
         variables = {
             "groupGuid": str(self.group.guid),
             "filters": [{
@@ -236,55 +183,27 @@ class FilterUsersTestCase(ElasticsearchTestCase):
                 "values": ["select_value_1"]
             }]
         }
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
 
         self.assertEqual(data['members']['total'], 1, msg="Expected one result")
         self.assertEqual(data['members']['edges'][0]['user']['guid'], self.member1.guid)
 
     def test_query_should_respect_offset(self):
-        query = """
-            query MembersQuery ($groupGuid: String!, $offset: Int) {
-                members(groupGuid: $groupGuid, offset: $offset) {
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-        self.initialize_index()
-
         variables = {
             "groupGuid": str(self.group.guid),
             "offset": 2
         }
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
 
         self.assertEqual(data['members']['edges'][0]['role'], 'member')
         self.assertEqual(len(data['members']['edges']), 3)
 
     def test_query_should_respect_limit(self):
-        query = """
-            query MembersQuery ($groupGuid: String!, $limit: Int) {
-                members(groupGuid: $groupGuid, limit: $limit) {
-                    edges {
-                        role
-                        user {
-                            guid
-                        }
-                    }
-                }
-            }
-        """
-        self.initialize_index()
-
         variables = {
             "groupGuid": str(self.group.guid),
             "limit": 2
         }
-        data = self.graphql_sync_data(query, variables, self.owner)
+        data = self.graphql_sync_data(self.query, variables, self.owner)
 
         self.assertEqual(data['members']['edges'][0]['role'], 'owner')
         self.assertEqual(data['members']['edges'][1]['role'], 'admin')
