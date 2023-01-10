03ed12288c6bb5fae06222c67c9238165b22db40
Jeroen Wellner <jeroen@wllnr.nl>
2023-01-10 15:25:46 +0000

    beheer#10924 update service worker for browsers

--- core/templates/service_worker.js
+++ core/templates/service_worker.js
@@ -3,7 +3,7 @@ self.addEventListener('push', function (event) {
     // Retrieve the textual payload from event.data (a PushMessageData object).
     // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
     // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-    let payload = event.data ? event.data.text() : { "head": "No Content", "body": "No Content", "icon": "" },
+    var payload = event.data ? event.data.text() : { "head": "No Content", "body": "No Content", "icon": "" },
         data = JSON.parse(payload),
         head = data.head,
         body = data.body,
@@ -11,7 +11,7 @@ self.addEventListener('push', function (event) {
 
     // If no url was received, it opens the home page of the website that sent the notification
     // Whitout this, it would open undefined or the service worker file.
-    url = data.url ? data.url : self.location.origin;
+    var url = data.url ? `${self.location.origin}${data.url}` : self.location.origin;
 
     // Keep the service worker alive until the notification is created.
     event.waitUntil(
@@ -26,9 +26,39 @@ self.addEventListener('push', function (event) {
 });
 
 self.addEventListener('notificationclick', function (event) {
-    event.waitUntil(
-        event.preventDefault(),
-        event.notification.close(),
-        self.clients.openWindow(event.notification.data.url)
-    );
+    event.notification.close();
+
+    if (event.notification.data.url) {
+        var targetUrl = new URL(event.notification.data.url);
+
+        // Attempt to fetch open tabs
+        event.waitUntil(self.clients.matchAll({ type: 'window' }).then(function (clientList) {
+            // No match by default
+            var matchFound = false;
+
+            // Traverse clients list
+            for (var client of clientList) {
+                // Parse url
+                var clientUrl = new URL(client.url);
+
+                // Check host matches
+                if (clientUrl.host === targetUrl.host && 'focus' in client) {
+                    // Update URL
+                    client.navigate(url);
+
+                    // Focus existing window
+                    client.focus();
+
+                    // Avoid opening a new window
+                    noMatch = true;
+                }
+            }
+
+            // If no open tabs, or none match the host, open a new window
+            if (!matchFound) {
+                event.waitUntil(self.clients.openWindow(url));
+            }
+        }));
+    }
+
 })
