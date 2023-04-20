c067b8079ae6e7ee988db819317461133663ea99
Erlend ter Maat <erlend@pleio.nl>
2023-04-20 13:21:14 +0200

    improve migration of attachments

--- core/post_deploy/translate_attachment_to_filefolder.py
+++ core/post_deploy/translate_attachment_to_filefolder.py
@@ -1,14 +1,14 @@
 import json
 import os
 import re
-import shutil
 import uuid
-from traceback import format_exc
 
+import kombu
+from celery import shared_task, chord
 from celery.utils.log import get_task_logger
-from django.core.files.base import ContentFile
 from django.db.models import Q
 from django.utils import timezone
+from django_tenants.utils import schema_context
 from post_deploy import post_deploy_action
 
 from blog.models import Blog
@@ -48,54 +48,55 @@ def process_revisions():
         migrate_revision(revision)
 
 
-def migrate_attachment(attachment_id):
+@shared_task
+def migrate_attachment(schema_name, attachment_id):
     # Create a filefolder with the same guid
-    attachment = None
-    try:
-        attachment = Attachment.objects.get(pk=attachment_id)
-        assert os.path.exists(attachment.upload.path), "Attachment does not exist"
+    with schema_context(schema_name):
+        attachment = None
+        try:
+            attachment = Attachment.objects.get(pk=attachment_id)
+            assert os.path.exists(attachment.upload.path), "Attachment does not exist"
 
-        file, _ = FileFolder.objects.get_or_create(
-            id=attachment.id,
-            type=FileFolder.Types.FILE,
-            owner=attachment.owner,
-        )
-        file.title = os.path.basename(attachment.upload.name)
-        file.created_at = attachment.created_at
-        file.updated_at = timezone.now()
-        file.upload = attachment.upload
-        file.write_access = [ACCESS_TYPE.user.format(file.owner)]
-        file.save()
+            file, _ = FileFolder.objects.get_or_create(
+                id=attachment.id,
+                type=FileFolder.Types.FILE,
+                owner=attachment.owner,
+            )
+            file.title = os.path.basename(attachment.upload.name)
+            file.created_at = attachment.created_at
+            file.updated_at = timezone.now()
+            file.upload = attachment.upload
+            file.write_access = [ACCESS_TYPE.user.format(file.owner)]
+            file.save()
 
-        if file.resized_images.get_queryset().count() == 0:
-            for image in attachment.resized_images.get_queryset():
-                image.original = file
-                image.save()
+            if file.resized_images.get_queryset().count() == 0:
+                for image in attachment.resized_images.get_queryset():
+                    image.original = file
+                    image.save()
 
-        if attachment.attached:
-            # Create File Reference.
-            container = _maybe_entity_object(attachment.attached)
-            reference, _ = FileReference.objects.get_or_create(file=file, container=container)
-            FileReference.objects.filter(pk=reference.pk).update(created_at=attachment.created_at)
+            container = _maybe_entity_object(attachment.attached_object_id)
+            if container:
+                # Create File Reference.
+                reference, _ = FileReference.objects.get_or_create(file=file, container=container)
+                FileReference.objects.filter(pk=reference.pk).update(created_at=attachment.created_at)
 
-        # Remove the attachment, but not the file on the disk.
-        Attachment.objects.filter(pk=attachment_id).update(upload=None)
-        attachment.refresh_from_db()
-        attachment.delete()
+            # Remove the attachment, but not the file on the disk.
+            Attachment.objects.filter(pk=attachment_id).update(upload=None)
+            attachment.refresh_from_db()
+            attachment.delete()
 
-        return file
-    except Exception as e:
-        print(format_exc())
-        if attachment:
-            logger.error("migrate_attachment error: %s %s@%s %s %s", tenant_schema(), attachment.name, attachment.attached, e.__class__, str(e))
-        else:
-            logger.error("migrate_attachment error: %s %s@%s %s %s", tenant_schema(), attachment_id, '???', e.__class__, str(e))
+            return file
+        except kombu.exceptions.EncodeError:
+            pass
+        except Exception as e:
+            if attachment:
+                logger.error("migrate_attachment error: %s %s@%s %s %s", tenant_schema(), attachment.name, attachment.attached_object_id, e.__class__, str(e))
+            else:
+                logger.error("migrate_attachment error: %s %s@%s %s %s", tenant_schema(), attachment_id, '???', e.__class__, str(e))
 
 
-def _maybe_entity_object(entity):
-    if not isinstance(entity, (Entity,)):
-        return entity
-    return Entity.objects.filter(pk=entity.pk).select_subclasses().first()
+def _maybe_entity_object(entity_id):
+    return Entity.objects.filter(pk=entity_id).select_subclasses().first()
 
 
 def persist_files_from_settings():
@@ -144,14 +145,16 @@ def persist_other_files():
 def process_attachments():
     attachment_ids = [str(pk) for pk in Attachment.objects.all().values_list('id', flat=True)]
     for attachment in attachment_ids:
-        migrate_attachment(attachment)
+        yield migrate_attachment.si(tenant_schema(), attachment)
 
 
-def schedule_calculate_checksum():
-    from core.tasks.misc import update_file_checksum
-    for file in FileFolder.objects.filter_attachments():
-        if file.is_image():
-            update_file_checksum.delay(tenant_schema(), file.guid)
+@shared_task
+def schedule_calculate_checksum(schema_name):
+    with schema_context(schema_name):
+        from core.tasks.misc import update_file_checksum
+        for file in FileFolder.objects.filter_attachments():
+            if file.is_image():
+                update_file_checksum.delay(tenant_schema(), file.guid)
 
 
 @post_deploy_action(auto=True)
@@ -163,8 +166,7 @@ def task():
         return
 
     process_revisions()
-    process_attachments()
-    schedule_calculate_checksum()
+    chord([*process_attachments()], schedule_calculate_checksum.si(tenant_schema())).apply_async()
 
     persist_files_from_settings()
     persist_group_icons()
